9ca28f1ace2d7cd3e60873d58156fb8e237e6c34
Pablo Silva <pablonicolassilvabravo@gmail.com>
2022-04-02 23:08:42 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 
 function App() {
   return (
@@ -10,8 +9,38 @@ function App() {
           <hr />
           <p class="lead">Welcome! This website contains some features that I develop in my free time.
             If you need contact me, write me to <a href="mailto:pablonicolassilvabravo@gmail.com" class="link-primary">my email</a>
-             :) </p>
+            :) </p>
           <p></p>
+        </div>
+
+
+        <div class="container">
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+              </tr>
+              <tr>
+                <th >2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+              </tr>
+              <tr>
+                <th >3</th>
+                <td >Larry the Bird</td>
+                <td>twitter</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </main>
     </div>
