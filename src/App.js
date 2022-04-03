a8e4e9f01d9953cf6d090ee903d2592654ed6bd4
Pablo Silva <pablonicolassilvabravo@gmail.com>
2022-04-02 23:00:43 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main class="flex-shrink-0">
+        <div class="container">
+          <h1 class="mt-5">Pablo Silva</h1>
+          <h5 class="mt-3">Backend Developer</h5>
+          <hr />
+          <p class="lead">Welcome! This website contains some features that I develop in my free time.
+            If you need contact me, write me to <a href="mailto:pablonicolassilvabravo@gmail.com" class="link-primary">my email</a>
+             :) </p>
+          <p></p>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
