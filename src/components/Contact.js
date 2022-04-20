0840601a3314b60be694e7a5a2624cc3f5af6de2
Pablo Silva <pablonicolassilvabravo@gmail.com>
2022-04-20 01:08:07 -0400

    remove api's

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,90 +2,11 @@ import { useState } from "react";
 
 
 const Contact = () => {
-    const [name, setName] = useState("");
-    const [contact, setContact] = useState("");
-    const [emailSubject, setEmailSubject] = useState("");
-    const [emailMessage, setEmailMessage] = useState("");
-
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-
-        await fetch("https://demo-backend-spring-boot.herokuapp.com/email/", {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            method: "POST",
-            body: JSON.stringify({
-                name: name,
-                contact: contact,
-                subject: emailSubject,
-                text: emailMessage,
-            }),
-        }).then((res) => {
-            alert('Thanks you :)')
-        });
-    }
+   
 
     return (
         <div className="App">
-            <main class="flex-shrink-0">
-                <div class="container">
-                    <h1 class="mt-5">Contact me</h1>
-                    <hr />
-
-                </div>
-            </main>
-
-            <p>
-                If you need contact me, write me
-            </p>
-
-            <form onSubmit={handleSubmit}>
-                <div class="form-group">
-                    <label>Name</label>
-                    <input
-                        className="form-control"
-                        type="text"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                    />
-                </div>
-                <br /> <div class="form-group">
-                    <label>Contact</label>
-                    <input
-                        className="form-control"
-                        type="text"
-                        value={contact}
-                        onChange={(e) => setContact(e.target.value)}
-                    />
-                </div>
-                <br />
-                <div class="form-group">
-                    <label>Subject</label>
-                    <input
-                        className="form-control"
-                        type="text"
-                        value={emailSubject}
-                        onChange={(e) => setEmailSubject(e.target.value)}
-                    />
-                </div>
-                <br />
-                <div class="form-group">
-                    <label>Message</label>
-
-                    <textarea
-                        className="form-control"
-                        type="text"
-                        rows="3"
-                        value={emailMessage}
-                        onChange={(e) => setEmailMessage(e.target.value)}
-                    />
-                </div>
-                <hr />
-                <button type="submit" className="btn btn-primary">Send</button>
-            </form>
-
+            <p>Contact</p>
         </div>
     );
 };
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,57 +2,12 @@ import React, { useState, useEffect } from 'react';
 
 const Project = () => {
 
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [projects, setProjects] = useState([]);
-
-    useEffect(() => {
-        fetch("https://demo-backend-spring-boot.herokuapp.com/api/v1/projects/")
-            .then(res => res.json())
-            .then(
-                (data) => {
-                    setIsLoaded(true);
-                    setProjects(data);
-                },
-                (error) => {
-                    setIsLoaded(true);
-                    setError(error);
-                }
-            )
-    }, [])
-
-    if (error) {
-        return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-        return <div class="spinner-border" role="status">
-            <span class="visually-hidden">Loading...</span>
-        </div>;
-    } else {
-        return (
-            <div>
-                <main class="flex-shrink-0">
-                    <div class="container">
-                        <h1 class="mt-5">My projects</h1>
-                        <hr />
-
-                    </div>
-                </main>
-                <table class="table">
-                    {projects.map(project => (
-                        <tr>
-                            <td key={project.id}>
-                                <a href={project.url} target="_blank">{project.name}
-                                </a>
-                            </td>
-                            <td key={project.id}>TODO
-                            </td>
-                        </tr>
-                    ))}
-                </table>
-            </div>
-
-        );
-    }
+    
+    return (
+        <div className="App">
+            <p>Projects</p>
+        </div>
+    );
 }
 
 
